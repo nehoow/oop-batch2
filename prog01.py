633f319da0c67d72eef4d88c4e6ee0fe0ef43318
Nyks <redgo3666@gmail.com>
2025-03-07 18:38:34 +0800

    removed notes

--- prog01.py
+++ prog01.py
@@ -60,11 +60,9 @@ for i in range(100):
         print (i)
     num+1
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
-# Ask user for two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-# Determine the range direction
 if num1 < num2:
     for i in range(num1 + 1, num2):
         print(i, end=" ")
